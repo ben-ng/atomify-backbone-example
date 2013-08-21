f77ab61c5a96edb16ee191723db72bd08af322de
Ben Ng <me@benng.me>
2013-08-21 09:23:43 -0600

    Update minifyify to 0.4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http')
   , st = require('st')
   , mount = st({ path: __dirname + '/static', url: '/static' })
   , fs = require('fs')
+  , path = require('path')
 
 var cssVariables = {
   lightBG: '#fdf6e3'
@@ -14,10 +15,14 @@ var cssVariables = {
 
 var atom = atomify({
   js: {
-    entry: './ui/entry.js'
+    entry: path.join(__dirname, 'ui', 'entry.js')
   , shim: {
       jquery: { path: __dirname + '/static/js/jquery.js', exports: '$' }
     }
+  , debug: true
+  , file: '/bundle.js'
+  , map: '/bundle.map'
+  , compressPaths: function (p) { return path.relative(__dirname, p) }
   }
 , css: {
     entry: './ui/entry.css'
@@ -31,6 +36,7 @@ http.createServer(function(req, res) {
   else {
     if (req.url == '/bundle.js') atom.js(req, res)
     else if (req.url == '/bundle.css') atom.css(req, res)
+    else if (req.url == '/bundle.map') atom.map(req, res)
     else if (req.url == '/') {
       var s = fs.createReadStream(__dirname + '/index.html')
       s.on('open', function () {
